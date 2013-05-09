1c4fd13a5d5ff9032e84050fd05c453e173ce3b0
Grant David Bachman <grantbachman@gmail.com>
2013-05-09 12:32:27 -0400

    Retrieve basic stock data

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask
+from pandas.io.data import DataReader
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    goog = DataReader("GOOG",  "yahoo", datetime(2000,1,1), datetime(2012,1,1))
+    return str(goog["Adj Close"].describe()["mean"])
