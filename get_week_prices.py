2ac29335fb1eb4dc6741b88bdfe28f9e2fdcc835
Grant David Bachman <grantbachman@gmail.com>
2013-05-24 16:36:23 -0400

    run get_week_prices.py every day, only execute if it's sunday...Damn you Heroku Scheduler

--- get_week_prices.py
+++ get_week_prices.py
@@ -1,5 +1,8 @@
 import soroban as s
+from datetime import datetime, timedelta
 
 if __name__ == '__main__':
-	lower, higher = s.calc(from_file=False)
-	s.save_all_stocks(lower,higher)
+	est = (datetime.utcnow() + timedelta(hours=-4)).timetuple()
+	if est[6] == 6:
+		lower, higher = s.calc(from_file=False)
+		s.save_all_stocks(lower,higher)
